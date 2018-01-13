c99930bd75ad5cc453d4c98883c705f0c2a4d9c0
Stepan Lutsenko <lustgr@yandex.ru>
2018-01-13 12:34:10 +0400

    [add] activities

---
+++ app/src/main/java/com/elewise/nlsvm/move2win/RoomsActivity.java
@@ -0,0 +1,13 @@
+package com.elewise.nlsvm.move2win;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class RoomsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rooms);
+    }
+}
